519889d8b0ff1a624ae3139d39da33b3bdfad9fd
ncr00t <geritix@mail.ru>
2018-11-10 00:17:18 +0300

    removed TaskInWorkRepository and TaskInWork

--- src/main/java/com/company/tasks/dao/TaskInWorkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.tasks.dao;
-
-import com.company.tasks.models.TaskInWork;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskInWorkRepository extends CrudRepository<TaskInWork, Integer> {
-}
