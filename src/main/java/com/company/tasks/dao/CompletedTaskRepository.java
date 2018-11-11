8425551444c305c586ffb8d1682b2a5f7e47d402
ncr00t <geritix@mail.ru>
2018-11-11 04:19:21 +0300

    removed not used classes for completed task

--- src/main/java/com/company/tasks/dao/CompletedTaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.company.tasks.dao;
-
-import com.company.tasks.models.CompletedTask;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CompletedTaskRepository extends CrudRepository<CompletedTask,Integer> {
-
-}
