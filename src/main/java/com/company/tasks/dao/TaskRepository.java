fafa4f64ec866fbb7087b29b27cfe58c7b31dcfb
ncr00t <geritix@mail.ru>
2018-11-14 01:20:11 +0300

    added Repository annotation

--- src/main/java/com/company/tasks/dao/TaskRepository.java
+++ src/main/java/com/company/tasks/dao/TaskRepository.java
@@ -2,10 +2,12 @@ package com.company.tasks.dao;
 
 import com.company.tasks.models.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends CrudRepository<Task, Integer> {
     List<Task> findByNameContaining(String taskName);
     List<Task> findByDateCreated(Date date);
