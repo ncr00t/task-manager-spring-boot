7ce7206a6f2b09832ad2a83eb86bc99352ba4412
ncr00t <geritix@mail.ru>
2018-11-16 23:01:20 +0300

    added tests to check the work requests

--- src/test/java/WebAppTest.java
+++ src/test/java/WebAppTest.java
@@ -1,4 +1,3 @@
-import com.company.tasks.controllers.TasksController;
 import com.company.tasks.models.Task;
 import com.company.tasks.services.TaskService;
 import org.junit.Before;
@@ -6,7 +5,6 @@ import org.junit.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,47 +14,126 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-public class WebAppControllerTest extends ApplicationTest{
+public class WebAppTest extends ApplicationTest{
+
+    private MockMvc mockMvc;
+
     @MockBean
     private TaskService taskService;
-    private TasksController tasksController;
-    private MockMvc mockMvc;
+
     @Autowired
     private WebApplicationContext webApplicationContext;
 
     @Before
     public void setUp() throws Exception {
-        tasksController = new TasksController(taskService);
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
-    public void taskController_should_be_not_null(){
-        assertNotNull(tasksController);
-    }
-
-    @Test
-    public void testGetPerson() throws Throwable {
-        // When
-        int id = 0;
-        ResultActions actions = mockMvc.perform(get("/updateTask?id=" + id));
-
-        // Then
-        actions.andExpect(status().isOk());
-    }
-
-    @Test
-    public void testTasks() throws Exception {
+    public void if_valid_request_for_get_all_tasks_then_status_request_should_be_ok() throws Exception {
         Collection<Task> getAllTasksResult = new ArrayList<>();
         doReturn(getAllTasksResult).when(taskService).findAllTasks();
-        ResultActions actions = mockMvc.perform(get("/tasks"));
+
+        String validRequestForGetAllTasks = "/tasks";
+        ResultActions actionsForGetAllTasks = mockMvc.perform(get(validRequestForGetAllTasks));
+        actionsForGetAllTasks
+                            .andExpect(
+                                    status()
+                                            .isOk());
 
     }
+
+    @Test
+    public void if_invalid_request_for_get_all_tasks_then_status_request_should_be_not_found() throws Exception {
+        Collection<Task> getAllTasksResult = new ArrayList<>();
+        doReturn(getAllTasksResult).when(taskService).findAllTasks();
+
+        String invaidRequestForGetAllTasks ="/taskss";
+        ResultActions actionsForGetAllTasks = mockMvc.perform(get(invaidRequestForGetAllTasks));
+        actionsForGetAllTasks
+                             .andExpect(
+                                     status()
+                                             .isNotFound());
+
+    }
+
+    @Test
+    public void if_valid_request_for_get_completed_tasks_then_status_request_should_be_ok() throws Exception {
+        Collection<Task> getCompletedTasksResult = new ArrayList<>();
+        doReturn(getCompletedTasksResult).when(taskService).findAllCompletedTasks();
+
+        String validRequestForGetCompletedTasks = "/completedTasks";
+        ResultActions actionsForGetCompletedTasks = mockMvc.perform(get(validRequestForGetCompletedTasks));
+        actionsForGetCompletedTasks
+                                   .andExpect(
+                                              status()
+                                                      .isOk());
+
+    }
+
+    @Test
+    public void if_invalid_request_for_get_completed_tasks_then_status_request_should_be_not_found() throws Exception {
+        Collection<Task> getCompletedTasksResult = new ArrayList<>();
+        doReturn(getCompletedTasksResult).when(taskService).findAllCompletedTasks();
+
+        String invalidRequestForGetCompletedTasks ="/completedTaskss";
+        ResultActions actionsForGetCompletedTasks = mockMvc.perform(get(invalidRequestForGetCompletedTasks));
+        actionsForGetCompletedTasks
+                                    .andExpect(
+                                               status()
+                                                       .isNotFound());
+
+    }
+
+    @Test
+    public void if_valid_request_for_get_tasks_in_work_then_status_request_should_be_ok() throws Exception {
+        Collection<Task> getTasksInWorkResult = new ArrayList<>();
+        doReturn(getTasksInWorkResult).when(taskService).findAllTasksInWork();
+
+        String validRequestForGetTasksInWork = "/tasksInWork";
+        ResultActions actionsForGetTasksInWork = mockMvc.perform(get(validRequestForGetTasksInWork));
+        actionsForGetTasksInWork
+                                .andExpect(
+                                           status()
+                                                   .isOk());
+
+    }
+
+    @Test
+    public void if_invalid_request_for_get_tasks_in_work_then_status_request_should_be_not_found() throws Exception {
+        Collection<Task> getTasksInWorkResult = new ArrayList<>();
+        doReturn(getTasksInWorkResult).when(taskService).findAllTasksInWork();
+
+        String invalidRequestForGetTasksInWork = "/tasksInWorkk";
+        ResultActions actionsForGetTasksInWork = mockMvc.perform(get(invalidRequestForGetTasksInWork));
+        actionsForGetTasksInWork
+                                .andExpect(
+                                            status()
+                                                    .isNotFound());
+
+    }
+
+    @Test
+    public void if_valid_request_for_show_statistics_complete_then_status_request_should_be_ok() throws Exception{
+        String validRequestForShowStatisticsComplete = "/statisticsComplete";
+        mockMvc.perform(get(validRequestForShowStatisticsComplete))
+               .andExpect(
+                          status()
+                                  .isOk());
+    }
+
+    @Test
+    public void if_invalid_request_for_show_statistics_complete_then_status_request_should_be_not_found() throws Exception{
+        String invalidRequestForShowStatisticsComplete = "/statisticsCompletee";
+        mockMvc.perform(get(invalidRequestForShowStatisticsComplete))
+               .andExpect(
+                          status()
+                                  .isNotFound());
+    }
+
 }
